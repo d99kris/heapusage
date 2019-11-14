8b0660d6b2de0751ee316576089dddf268c42a52
Kristofer Berggren <kristofer.berggren@gmail.com>
2019-11-14 21:51:08 +0800

    update man-page generation

--- genman.sh
+++ genman.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p build && cd build && cmake .. && make -s && \
-help2man -n "find memory leaks in applications" -N -o heapusage.1 ./heapusage
+cd src && \
+help2man -n "find memory leaks in applications" -N -o heapusage.1 ./heapusage && cd .. && \
+mkdir -p build && cd build && cmake .. && make -s
 exit ${?}
 
